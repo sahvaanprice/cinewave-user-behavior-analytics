49783171bca6281f89bebcce534270375eca6f5b
sahvaanprice <sahvaan.p26@gmail.com>
2026-02-26 21:31:33 -0500

    Update 07_backfill_plan_history.sql

--- sql/07_backfill_plan_history.sql
+++ sql/07_backfill_plan_history.sql
@@ -1,1 +1,21 @@
+------------------------------------------------------------
+-- Insert plan_history records
+------------------------------------------------------------
+INSERT INTO dbo.plan_history (
+    plan_change_id,
+    user_id,
+    change_time,
+    from_plan,
+    to_plan,
+    change_reason
+)
+SELECT
+    @CurrentPlanChangeID +
+    ROW_NUMBER() OVER (ORDER BY user_id),
 
+    user_id,
+    GETDATE(),
+    'Free',
+    'Premium',
+    'Engagement Upgrade'
+FROM #upgrades;
